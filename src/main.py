d450d5dc63b5609f59a6ac453b17189ceafa06ee
João Gomes <joaorgomes1@hotmail.com>
2020-11-14 12:28:18 +0000

    Started the GUI

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
 import random
-from time import sleep
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
+
+from tkinter import *
+from time import sleep
+from matplotlib.colors import ListedColormap
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.figure import Figure
+
 
 class Schelling:
 
@@ -16,12 +22,13 @@ class Schelling:
         ratios = [(1 - empty_ratio) / 2, (1 - empty_ratio) / 2, empty_ratio]
         # Actual size of population
         population_size = int(np.sqrt(self.size)) ** 2
+        print('Population size: ' +str(population_size))
         # Generate starting placement
         self.population = np.random.choice([-1, 1, 0], size=population_size, p=ratios)
         # Reshape 1D array to 2D
         self.population = np.reshape(self.population, (int(np.sqrt(self.size)), int(np.sqrt(self.size))))
 
-    def printBoard(self):
+    def printToConsole(self):
         print('-' * (len(self.population) * 2 + 2))
         for x in self.population:
             row_str = "| "
@@ -47,10 +54,6 @@ class Schelling:
                 y_max = min(col + self.neighbour_depth + 1, int(np.sqrt(self.size)))
                 neighbourhood = self.population[x_min:x_max, y_min:y_max]
 
-                # print("Row : " + str(row) + ", Col : " + str(col))
-                # print("x_min : " + str(x_min) + ", x_max : " + str(x_max) + ", y_min : " + str(y_min) + ", y_max : " + str(y_max))
-                # print(neighbourhood)
-
                 neighbourhood_size = np.size(neighbourhood)
                 number_empty_entities = len(np.where(neighbourhood == 0)[0]) # number of empty entities on the neighbourhood
                 if neighbourhood_size != number_empty_entities + 1: # plus the current node
@@ -62,16 +65,69 @@ class Schelling:
                         self.population[random_empty_entity] = race
                         self.population[row, col] = 0
 
-            
 
 round = 0
-schelling = Schelling(500, 0.3, 0.4, 1)
-print('---------> Round 0')
-schelling.printBoard()
+schelling = Schelling(100, 0.5, 0.9, 1)
 
-number_iterations = 100
+# print('---------> Round 0')
+# schelling.printBoard()
 
-for i in range(number_iterations):
-    print('---------> Round ' + str(i + 1))
-    schelling.run_round()
-    schelling.printBoard()
+number_iterations = 50
+
+# for i in range(number_iterations):
+#     print('---------> Round ' + str(i + 1))
+#     schelling.run_round()
+#     schelling.printBoard()
+
+
+
+
+root = Tk()
+root.title('Schelling\'s Model of Segregation')
+#root.geometry("1200x800")~
+root.bind("<Configure>", resize)
+
+left_frame = Frame(root)
+left_frame.pack(side=LEFT)
+right_frame = Frame(root)
+right_frame.pack(side=RIGHT)
+
+
+plt.style.use("ggplot")
+cmap = ListedColormap(['red', 'white', 'royalblue'])
+
+fig, ax = plt.subplots()
+
+ax.axis('off')
+ax.pcolor(schelling.population, cmap=cmap, edgecolors='w', linewidths=1) 
+
+canvas = FigureCanvasTkAgg(fig, master=root)
+canvas.draw()
+canvas.get_tk_widget().pack(side=LEFT, fill=BOTH, expand=True)
+
+toolbar = NavigationToolbar2Tk(canvas, root)
+toolbar.update()
+canvas._tkcanvas.pack(side=TOP, fill=BOTH, expand=True)
+
+
+
+
+def run_graph():
+    for i in range(number_iterations): 
+        schelling.run_round()
+        ax.pcolor(schelling.population, cmap=cmap, edgecolors='w', linewidths=1)
+        canvas.draw()
+
+my_button1 = Button(right_frame, text='Start!', command=run_graph)
+my_button1.pack()
+
+
+root.mainloop()
+
+            
+
+# refs
+# https://www.binpress.com/simulating-segregation-with-python/
+# https://ytliu0.github.io/schelling/
+# https://docs.python.org/3/library/tkinter.html
+# https://matplotlib.org/3.3.3/contents.html

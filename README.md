e991e46de14d5025216069d141f4428938ef9d68
João Gomes <joaorgomes1@hotmail.com>
2020-11-15 11:31:26 +0000

    GUI initial stage Updated README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# crc-project-2
+# CRC Project 2
+Projecto 2
+
+### Grupo 15
+- 89905, João Gomes
+- 98649, Pedro Guerra
+- 98668, Ricardo Gonçalves
+
+### References
+- Schelling, T.C., [Dynamic models of segregation](http://norsemathology.org/longa/classes/stuff/DynamicModelsOfSegregation.pdf). Journal of Mathematical Sociology, 1971. 1(2): p. 143-186
+- McCown, F.  ["Schelling's Model of Segregation"](http://nifty.stanford.edu/2014/mccown-schelling-model-segregation/) (2014).
+- Hart, V. and N. Case. [Parable of the polygons](https://ncase.me/polygons/): A playable post on the shape of society.
+- Sheong, S. [Simulating racial segregation with Go](https://towardsdatascience.com/simulating-racial-segregation-with-go-6224c253a1d2) (2019)
+- Moujahid, A. [An Implementation of Schelling Segregation Model using Python and Streamlit](http://adilmoujahid.com/posts/2020/05/streamlit-python-schelling/) (2020)
+- Moujahid, A. [An Introduction to Agent-Based Models: Simulating Segregation with Python](https://www.binpress.com/simulating-segregation-with-python/) (2020)
+- Liu, Y.T. [The Schelling Model of Segregation: Static and Dynamic Equilibrium](https://ytliu0.github.io/schelling/) (2017)
+- [Schelling’s Segregation Model](https://python.quantecon.org/schelling.html)
+- [tkinter documentation](https://docs.python.org/3/library/tkinter.html)
+- [matplotlib documentation](https://matplotlib.org/3.3.3/contents.html)
--- src/main.py
+++ src/main.py
@@ -13,6 +13,14 @@ class Schelling:
 
     # Only with 2 races for now, later it will be possible to send an array with tuples for each race
     def __init__(self, size, empty_ratio, neighbours_race_threshold, neighbour_depth, race1_ratio=None, race2_ratio=None):
+        self.model_configure(size, empty_ratio, neighbours_race_threshold, neighbour_depth)
+
+        self.cmap = ListedColormap(['red', 'white', 'royalblue'])
+
+        self.create_plot()
+
+    
+    def model_configure(self, size, empty_ratio, neighbours_race_threshold, neighbour_depth):
         self.size = size
         self.empty_ratio = empty_ratio
         self.neighbours_race_threshold = neighbours_race_threshold
@@ -22,13 +30,14 @@ class Schelling:
         ratios = [(1 - empty_ratio) / 2, (1 - empty_ratio) / 2, empty_ratio]
         # Actual size of population
         population_size = int(np.sqrt(self.size)) ** 2
-        print('Population size: ' +str(population_size))
+        print('Population size: ' + str(population_size))
         # Generate starting placement
         self.population = np.random.choice([-1, 1, 0], size=population_size, p=ratios)
         # Reshape 1D array to 2D
         self.population = np.reshape(self.population, (int(np.sqrt(self.size)), int(np.sqrt(self.size))))
 
-    def printToConsole(self):
+
+    def print_to_console(self):
         print('-' * (len(self.population) * 2 + 2))
         for x in self.population:
             row_str = "| "
@@ -56,9 +65,11 @@ class Schelling:
 
                 neighbourhood_size = np.size(neighbourhood)
                 number_empty_entities = len(np.where(neighbourhood == 0)[0]) # number of empty entities on the neighbourhood
+                
                 if neighbourhood_size != number_empty_entities + 1: # plus the current node
                     number_similar = len(np.where(neighbourhood == race)[0]) - 1
                     similarity_ratio = number_similar / (neighbourhood_size - number_empty_entities - 1) 
+                    
                     if similarity_ratio < self.neighbours_race_threshold: # unhappy
                         empty_entities = list(zip(np.where(self.population == 0)[0], np.where(self.population == 0)[1]))
                         random_empty_entity = random.choice(empty_entities)
@@ -66,68 +77,169 @@ class Schelling:
                         self.population[row, col] = 0
 
 
-round = 0
-schelling = Schelling(100, 0.5, 0.9, 1)
+    def create_plot(self):
+        plt.style.use("ggplot")
 
-# print('---------> Round 0')
-# schelling.printBoard()
+        self.fig, self.ax = plt.subplots()
 
-number_iterations = 50
-
-# for i in range(number_iterations):
-#     print('---------> Round ' + str(i + 1))
-#     schelling.run_round()
-#     schelling.printBoard()
+        self.ax.axis('off')
+        self.ax.pcolor(self.population, cmap=self.cmap, edgecolors='w', linewidths=1) 
 
 
+class Application(Tk):
+
+    def __init__(self):
+        Tk.__init__(self)
+
+        self.title('Schelling\'s Model of Segregation')
+
+        self.left_frame = Frame(self)
+        self.left_frame.pack(side=LEFT, fill=BOTH, expand=True)
+        self.right_frame = Frame(self)
+        self.right_frame.pack(side=RIGHT, padx=10, pady=10, fill=BOTH)
+
+        self.create_text_box_frames()
+
+        self.create_button_row_frame()
+
+    def create_text_box_frames(self):
+        # create iteration frame
+        iteration_frame = Frame(self.right_frame)
+
+        iteration_label = Label(iteration_frame, text="Number of Iterations ")
+        iteration_label.pack(side=LEFT)
+
+        iteration_value = IntVar()
+        self.iteration_box = Entry(iteration_frame, textvariable=iteration_value)
+        self.iteration_box.pack(side=LEFT)
+
+        iteration_frame.pack(side=TOP)
 
 
-root = Tk()
-root.title('Schelling\'s Model of Segregation')
-#root.geometry("1200x800")~
-root.bind("<Configure>", resize)
+        # create population frame
+        population_frame = Frame(self.right_frame)
 
-left_frame = Frame(root)
-left_frame.pack(side=LEFT)
-right_frame = Frame(root)
-right_frame.pack(side=RIGHT)
+        population_label = Label(population_frame, text="Population Size ")
+        population_label.pack(side=LEFT)
+
+        population_value = IntVar()
+        self.population_box = Entry(population_frame, textvariable=population_value)
+        self.population_box.pack(side=LEFT)
+
+        population_frame.pack(side=TOP)
 
 
-plt.style.use("ggplot")
-cmap = ListedColormap(['red', 'white', 'royalblue'])
+        # create empty ratio
+        empty_ratio_frame = Frame(self.right_frame)
 
-fig, ax = plt.subplots()
+        empty_ratio_label = Label(empty_ratio_frame, text="Empty Ratio ")
+        empty_ratio_label.pack(side=LEFT)
 
-ax.axis('off')
-ax.pcolor(schelling.population, cmap=cmap, edgecolors='w', linewidths=1) 
+        self.empty_ratio_box = Scale(empty_ratio_frame, from_=0.00, to=1.00, digits=3, resolution=0.01, orient=HORIZONTAL)
+        self.empty_ratio_box.pack(side=LEFT)
 
-canvas = FigureCanvasTkAgg(fig, master=root)
+        empty_ratio_frame.pack(side=TOP)
+
+        # create races
+        races_frame = Frame(self.right_frame)
+
+        race1_frame = Frame(races_frame)
+        race1_label = Label(race1_frame, text="Race 1 ")
+        race1_label.pack(side=LEFT)
+        self.race1_ratio_box = Scale(race1_frame, from_=0.00, to=1.00, digits=3, resolution=0.01, orient=HORIZONTAL)
+        self.race1_ratio_box.pack(side=LEFT)
+        race1_frame.pack(side=TOP)
+
+        race2_frame = Frame(races_frame)
+        race2_label = Label(race2_frame, text="Race 2 ")
+        race2_label.pack(side=LEFT)
+        self.race2_ratio_box = Scale(race2_frame, from_=0.00, to=1.00, digits=3, resolution=0.01, orient=HORIZONTAL)
+        self.race2_ratio_box.pack(side=LEFT)
+        race2_frame.pack(side=TOP)
+
+        race3_frame = Frame(races_frame)
+        race3_label = Label(race3_frame, text="Race 3 ")
+        race3_label.pack(side=LEFT)
+        self.race3_ratio_box = Scale(race3_frame, from_=0.00, to=1.00, digits=3, resolution=0.01, orient=HORIZONTAL)
+        self.race3_ratio_box.pack(side=LEFT)
+        race3_frame.pack(side=TOP)
+
+        race4_frame = Frame(races_frame)
+        race4_label = Label(race4_frame, text="Race 4 ")
+        race4_label.pack(side=LEFT)
+        self.race4_ratio_box = Scale(race4_frame, from_=0.00, to=1.00, digits=3, resolution=0.01, orient=HORIZONTAL)
+        self.race4_ratio_box.pack(side=LEFT)
+        race4_frame.pack(side=TOP)
+
+        race5_frame = Frame(races_frame)
+        race5_label = Label(race5_frame, text="Race 5 ")
+        race5_label.pack(side=LEFT)
+        self.race5_ratio_box = Scale(race5_frame, from_=0.00, to=1.00, digits=3, resolution=0.01, orient=HORIZONTAL)
+        self.race5_ratio_box.pack(side=LEFT)
+        race5_frame.pack(side=TOP)
+        
+        races_frame.pack(side=TOP)
+
+        # create similarity threshold
+        similarity_threshold_frame = Frame(self.right_frame)
+
+        similarity_threshold_label = Label(similarity_threshold_frame, text="Similarity Threshold ")
+        similarity_threshold_label.pack(side=LEFT)
+
+        self.similarity_threshold_box = Scale(similarity_threshold_frame, from_=0.00, to=1.00, digits=3, resolution=0.01, orient=HORIZONTAL)
+        self.similarity_threshold_box.pack(side=LEFT)
+
+        similarity_threshold_frame.pack(side=TOP)
+    
+        
+
+    def create_button_row_frame(self):
+        self.button_row_frame = Frame(self.right_frame)
+        self.button_row_frame.pack(side=BOTTOM, pady=20)
+
+        self.start_button = Button(self.button_row_frame, text='Start')
+        self.start_button.pack(side=LEFT)
+        self.stop_button = Button(self.button_row_frame, text='Stop')
+        self.stop_button.pack(side=LEFT)
+        self.reset_button = Button(self.button_row_frame, text='Reset')
+        self.reset_button.pack(side=LEFT)
+        self.step_button = Button(self.button_row_frame, text='Step')
+        self.step_button.pack(side=LEFT)
+
+    def _quit():
+        self.quit()  
+        self.destroy()
+
+
+number_iterations = 20
+
+schelling = Schelling(100, 0.4, 0.8, 1)
+
+
+app = Application()
+
+canvas = FigureCanvasTkAgg(schelling.fig, master=app.left_frame)
 canvas.draw()
 canvas.get_tk_widget().pack(side=LEFT, fill=BOTH, expand=True)
 
-toolbar = NavigationToolbar2Tk(canvas, root)
-toolbar.update()
-canvas._tkcanvas.pack(side=TOP, fill=BOTH, expand=True)
-
-
-
+# Key handler
+# def on_key_press(event):
+#     print("you pressed {}".format(event.key))
+#     key_press_handler(event, canvas)
+# canvas.mpl_connect("key_press_event", on_key_press)
 
 def run_graph():
     for i in range(number_iterations): 
         schelling.run_round()
-        ax.pcolor(schelling.population, cmap=cmap, edgecolors='w', linewidths=1)
+        schelling.ax.pcolor(schelling.population, cmap=schelling.cmap, edgecolors='w', linewidths=1)
         canvas.draw()
 
-my_button1 = Button(right_frame, text='Start!', command=run_graph)
-my_button1.pack()
+def print_values():
+    print("xd")
+    print(app.population_box.get())
+    print(app.iteration_box.get())
 
+app.start_button.configure(command=run_graph)
+app.step_button.configure(command=print_values)
 
-root.mainloop()
-
-            
-
-# refs
-# https://www.binpress.com/simulating-segregation-with-python/
-# https://ytliu0.github.io/schelling/
-# https://docs.python.org/3/library/tkinter.html
-# https://matplotlib.org/3.3.3/contents.html
+app.mainloop()
